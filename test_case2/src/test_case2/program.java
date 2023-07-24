a0667346a7a8e31d53043bc92024364d81457da9
JANANI <JANANI@platform.wondershare.com>
2023-07-24 15:10:02 +0530

    Array program

---
+++ test_case2/src/test_case2/program.java
@@ -0,0 +1,13 @@
+package test_case2;
+
+public class program {
+
+	public static void main(String[] args) {
+		int[] arr = new int[2];
+        arr[0] = 10;
+        arr[1] = 20;
+ 
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+    }
+}
